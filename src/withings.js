2ed24ea2f79fa4559434d3e0214e3084d353b530
LiveSlowSkateFast <jp@jprowan.com>
2019-11-02 17:23:21 -0700

    use dynamo to manage withings tokens

--- src/withings.js
+++ src/withings.js
@@ -1,16 +1,76 @@
 const fetch = require('node-fetch')
+const { URLSearchParams } = require('url')
 const dotenv = require('dotenv')
+const AWS = require('aws-sdk')
 
 dotenv.config()
+
+AWS.config.update({ region: 'us-west-2' })
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+
+const getNewTokens = async (refreshToken) => {
+
+  const postParams = new URLSearchParams()
+  postParams.append('grant_type', 'refresh_token')
+  postParams.append('client_id', process.env.WITHINGS_CLIENT_ID)
+  postParams.append('client_secret', process.env.WITHINGS_CLIENT_SECRET)
+  postParams.append('refresh_token', refreshToken)
+  postParams.append('redirect_uri', 'https://sync.jprowan.com/withings/callback')
+
+  const response = await fetch("https://account.withings.com/oauth2/token", {
+    method: "POST",
+    body: postParams
+  })
+  console.log(response.status)
+
+  const data = await response.json()
+  console.log(data)
+
+  const tokens = {
+    accessToken: data.access_token,
+    refreshToken: data.refresh_token,
+  }
+
+  return tokens
+}
+
+const getLastRefreshToken = async (userId) => {
+
+  const response = await ddb.get({
+    TableName: 'WithingsTokens',
+    Key: {
+      UserId: userId,
+    },
+  }).promise()
+
+  return response.Item.RefreshToken
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+
+  const response = await ddb.put({
+    TableName: 'WithingsTokens',
+    Item: {
+      UserId: userId,
+      RefreshToken: refreshToken,
+    }
+  }).promise()
+
+  return response
+}
 
 exports.getWeight = async (event) => {
   console.log('Getting weight from withings.')
 
-  //TODO: Implement retrieving tokens from Mongo and refreshing before sending post to update.
+  const lastRefreshToken = await getLastRefreshToken(event.userId)
+  const newTokens = await getNewTokens(lastRefreshToken)
+
+  storeRefreshToken(event.userId, newTokens.refreshToken)
+
 
   const res = await fetch("https://wbsapi.withings.net/measure" +
     `?action=getmeas&meastype=1&startdate=${event.startDate}&enddate=${event.endDate}`, {
-    headers: {Authorization: `Bearer 439d3b1f3b77fee4da6c73272a4dd1af1ee70175`}
+    headers: {Authorization: `Bearer ${newTokens.accessToken}`}
   })
 
   const response = await res.json()
@@ -18,6 +78,6 @@ exports.getWeight = async (event) => {
   const grams = measurement.value
   const weight = Math.round((grams/453.592) * 100) / 100
 
-  console.log(weight)
   return weight
+
 }
